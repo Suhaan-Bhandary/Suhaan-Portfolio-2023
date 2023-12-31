273ca5574bbf55dd93f856cfe4dd0763616f233d
Suhaan-Bhandary <suhaanbhandary1@gmail.com>
2023-12-31 12:39:06 +0530

    feat: Made resume button and some changes

--- components/Header.tsx
+++ components/Header.tsx
@@ -26,25 +26,30 @@ const navLinks = [
 export default function Header() {
   return (
     <div className="container mx-auto flex items-center px-6 py-2 h-24">
-      <h1 className="font-bold">Portfolio</h1>
+      <h1 className="font-bold text-2xl">
+        <Link href="/">SB</Link>
+      </h1>
 
-      <div className="grow hidden md:flex">
-        <div className="grow">
+      <nav className="grow hidden md:flex">
+        <div className="flex justify-center items-center grow">
           <ul className="flex items-center justify-center gap-2 md:gap-8">
             {navLinks.map(({ name, href }) => (
               <li key={name}>
-                <Link href={href}>{name}</Link>
+                <Link href={href} className="hover:text-accent-400">
+                  {name}
+                </Link>
               </li>
             ))}
           </ul>
         </div>
 
-        <div className="hidden md:block">
-          <Link href="#" className="font-bold">
-            Resume
-          </Link>
-        </div>
-      </div>
+        <Link
+          href="#"
+          className="hidden md:block border px-4 py-2 text-sm border-accent-400 text-accent-400 rounded-sm"
+        >
+          Resume
+        </Link>
+      </nav>
 
       <button className="ml-auto md:hidden">Show</button>
     </div>
