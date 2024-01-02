a4f68878f3f554f1d00789d8f65911412ccc910d
Suhaan-Bhandary <suhaanbhandary1@gmail.com>
2024-01-02 15:19:28 +0530

    feat: added navbar to mobile screens

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,9 @@
+"use client";
+
 import Link from "next/link";
+import { useState } from "react";
+import { IoMdClose } from "react-icons/io";
+import { RxHamburgerMenu } from "react-icons/rx";
 
 const navLinks = [
   {
@@ -24,34 +29,55 @@ const navLinks = [
 ];
 
 export default function Header() {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsNavOpen((state) => !state);
+  };
+
   return (
-    <div className="fixed z-50 left-0 right-0 mx-auto flex items-center px-10 py-2 h-24 bg-bgBlack-800">
-      <h1 className="font-bold text-2xl">
-        <Link href="/">SB</Link>
-      </h1>
+    <>
+      {isNavOpen ? (
+        <div className="bg-bgBlack-400 bg-opacity-50 fixed inset-0 z-20"></div>
+      ) : null}
 
-      <nav className="grow hidden md:flex">
-        <div className="flex justify-center items-center grow">
-          <ul className="flex items-center justify-center gap-2 md:gap-8">
-            {navLinks.map(({ name, href }) => (
-              <li key={name}>
-                <Link href={href} className="hover:text-accent-400">
-                  {name}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </div>
+      <div className="fixed z-50 left-0 right-0 mx-auto flex items-center px-10 py-2 h-24 bg-bgBlack-800">
+        <h1 className="font-bold text-2xl">
+          <Link href="/">SB</Link>
+        </h1>
 
-        <Link
-          href="#"
-          className="hidden md:block border px-4 py-2 text-sm border-accent-400 text-accent-400 rounded-sm"
+        <nav
+          className={`bg-bgBlack-400 grow fixed top-24 h-0 text-opacity-0 md:opacity-1 overflow-hidden left-0 right-0 z-40 md:relative md:top-0 md:h-[100%] md:bg-bgBlack-800 md:flex md:justify-center md:items-center transition-all duration-200 ${
+            isNavOpen ? "h-[50%] text-opacity-1" : ""
+          }`}
         >
-          Resume
-        </Link>
-      </nav>
+          <div className="flex justify-center items-center grow">
+            <ul className="p-16 flex flex-col md:flex-row items-center justify-center gap-2 md:gap-8">
+              {navLinks.map(({ name, href }) => (
+                <li key={name}>
+                  <Link
+                    href={href}
+                    className="text-xl hover:text-accent-400 md:text-base"
+                  >
+                    {name}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
 
-      <button className="ml-auto md:hidden">Show</button>
-    </div>
+          <Link
+            href="#"
+            className="hidden md:block border px-4 py-2 text-sm border-accent-400 text-accent-400 rounded-sm"
+          >
+            Resume
+          </Link>
+        </nav>
+
+        <button className="ml-auto md:hidden text-2xl" onClick={toggleNavbar}>
+          {!isNavOpen ? <RxHamburgerMenu /> : <IoMdClose />}
+        </button>
+      </div>
+    </>
   );
 }
