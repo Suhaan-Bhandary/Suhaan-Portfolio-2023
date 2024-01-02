0842e3accfad9f86e0e600b047df491203817500
Suhaan-Bhandary <suhaanbhandary1@gmail.com>
2024-01-02 21:30:24 +0530

    Removal(loading): Removed loading animation

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,25 +1,3 @@
 export default function Loading() {
-  return (
-    <div className="fixed inset-0 flex justify-center items-center bg-bgBlack-800 z-50 text-textLight-800">
-      <svg viewBox="0 0 400 160" className="translate-y-[-10%]">
-        <text
-          x="50%"
-          y="50%"
-          dy="0.32em"
-          textAnchor="middle"
-          style={{
-            fontSize: "2rem",
-            stroke: "white",
-            strokeWidth: 2,
-            fontWeight: "700",
-            fill: "transparent",
-            letterSpacing: "4px-",
-            animation: "8s infinite alternate animate-stroke",
-          }}
-        >
-          Suhaan Bhandary.
-        </text>
-      </svg>
-    </div>
-  );
+  return null;
 }
