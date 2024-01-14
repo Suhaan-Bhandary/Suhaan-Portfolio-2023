8a8cab110061594330bed993e185a0fd141cef84
Suhaan-Bhandary <suhaanbhandary1@gmail.com>
2024-01-14 16:35:54 +0530

    fix: removed css color override and made it extendible

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,21 +13,21 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
-    },
-    colors: {
-      bgBlack: {
-        400: 'rgb(28, 28, 39)',
-        800: 'rgb(25, 25, 36)',
-      },
-      textLight: {
-        200: 'rgb(136, 146, 176)',
-        400: 'rgb(177, 178, 179)',
-        600: '#ccd6f6',
-        800: 'rgb(242, 243, 244)',
-      },
-      white: '#fff',
-      accent: {
-        400: '#97FFF4',
+      colors: {
+        bgBlack: {
+          400: 'rgb(28, 28, 39)',
+          800: 'rgb(25, 25, 36)',
+        },
+        textLight: {
+          200: 'rgb(136, 146, 176)',
+          400: 'rgb(177, 178, 179)',
+          600: '#ccd6f6',
+          800: 'rgb(242, 243, 244)',
+        },
+        white: '#fff',
+        accent: {
+          400: '#97FFF4',
+        },
       },
     },
     boxShadow: {
