de5c70c9010ebb1a16580494f3b4e907e0bbeb6e
Suhaan-Bhandary <suhaanbhandary1@gmail.com>
2024-01-14 15:31:06 +0530

    feat: Increased bg opacity for modal

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -15,7 +15,7 @@ export default function Modal({ children }: props) {
   const router = useRouter();
 
   return (
-    <div className="fixed inset-0 z-50 overflow-auto bg-bgBlack-800 bg-opacity-60 p-[1.5rem] transition-colors md:p-14">
+    <div className="fixed inset-0 z-50 overflow-auto bg-bgBlack-800 bg-opacity-70 p-[1.5rem] transition-colors md:p-14">
       <div className="shadow relative mx-auto max-w-[600px] overflow-auto rounded-xl bg-bgBlack-400 p-6 md:w-[80%]">
         <button
           className="absolute right-2 top-4 rounded-lg p-1 text-xl text-white hover:bg-opacity-50"
