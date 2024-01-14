974f726f50a7a92fe97573854a553bf7fc111b87
Suhaan-Bhandary <suhaanbhandary1@gmail.com>
2024-01-14 17:56:38 +0530

    fix: corrected the alt tag for image

--- app/project/[projectId]/opengraph-image.tsx
+++ app/project/[projectId]/opengraph-image.tsx
@@ -6,7 +6,7 @@ import { ImageResponse } from 'next/og';
 
 export const runtime = 'edge';
 
-export const alt = 'About Acme';
+export const alt = 'Project Detail - Suhaan Bhandary';
 export const size = {
   width: 1200,
   height: 630,
