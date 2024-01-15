60766363c00886f5b913a9f9aad8ec1c757cfab5
Suhaan-Bhandary <suhaanbhandary1@gmail.com>
2024-01-15 20:13:28 +0530

    fix(experience): made the li relative so that button can be absolute in terms of the li

--- app/@experience/page.tsx
+++ app/@experience/page.tsx
@@ -23,7 +23,7 @@ export default function Experience() {
             }) => (
               <li
                 key={`${joinDate}-${endDate}`}
-                className="top-0 mb-6 rounded-s bg-bgBlack-400 p-5 shadow-boxFade transition-all duration-500 last:mb-0 hover:shadow-boxFull lg:relative lg:mb-12 lg:w-1/2 lg:after:absolute lg:after:aspect-square lg:after:h-4 lg:after:rounded-full lg:after:bg-textLight-400 lg:after:content-[''] lg:odd:float-left lg:odd:clear-right lg:odd:translate-x-[-30px] lg:odd:after:right-[-30px] lg:odd:after:top-0 lg:odd:after:translate-x-[50%] lg:odd:after:translate-y-[-50%] lg:even:float-right lg:even:clear-left lg:even:translate-x-[30px] lg:even:after:left-[-30px] lg:even:after:top-0 lg:even:after:translate-x-[-50%] lg:even:after:translate-y-[-50%] lg:hover:after:bg-accent-400 lg:hover:after:outline lg:hover:after:outline-2"
+                className="relative top-0 mb-6 rounded-s bg-bgBlack-400 p-5 shadow-boxFade transition-all duration-500 last:mb-0 hover:shadow-boxFull lg:relative lg:mb-12 lg:w-1/2 lg:after:absolute lg:after:aspect-square lg:after:h-4 lg:after:rounded-full lg:after:bg-textLight-400 lg:after:content-[''] lg:odd:float-left lg:odd:clear-right lg:odd:translate-x-[-30px] lg:odd:after:right-[-30px] lg:odd:after:top-0 lg:odd:after:translate-x-[50%] lg:odd:after:translate-y-[-50%] lg:even:float-right lg:even:clear-left lg:even:translate-x-[30px] lg:even:after:left-[-30px] lg:even:after:top-0 lg:even:after:translate-x-[-50%] lg:even:after:translate-y-[-50%] lg:hover:after:bg-accent-400 lg:hover:after:outline lg:hover:after:outline-2"
               >
                 <div>
                   <p className="mb-3s text-sm font-medium tracking-wide text-textLight-400 lg:absolute lg:top-[-30px]">{`${joinDate} - ${endDate}`}</p>
